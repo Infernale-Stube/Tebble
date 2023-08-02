78af86646ee6ba571c0dd8eb1c11d9ca2b3adf2f
Alexander Ballauf <ballaual@gmail.com>
2023-08-02 12:58:59 +0200

    Refactor: implement stylesheet for menu

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import styles from "../styles/menuStyles"
 
 export default function Menu() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Menu Screen</Text>
     </View>
   );
