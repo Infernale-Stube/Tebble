e312b7c4028f1f2ccdee4523434903756082f4e7
Alexander Ballauf <ballaual@gmail.com>
2023-08-02 12:08:48 +0200

    Feat: setup screen directory

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+export { default as Menu } from "./Menu";
+export { default as Game } from "./Game";
+export { default as Settings } from "./Settings";
+export { default as Score } from "./Score";
+export { default as About } from "./About";
