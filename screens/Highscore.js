0d5dd208d59a4f8cc655e90ec284faaebe4cca9a
Alexander Ballauf <ballaual@gmail.com>
2023-08-03 15:43:54 +0200

    Feat: adding highscore screen/styles

---
+++ screens/Highscore.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { globalStyles, hishscoreStyles } from '../styles'
+
+export default function Highscore() {
+  return (
+    <View style={globalStyles.container}>
+      <Text>Highscore Screen</Text>
+    </View>
+  );
+}
--- screens/index.js
+++ screens/index.js
@@ -3,3 +3,4 @@ export { default as Game } from "./Game";
 export { default as Settings } from "./Settings";
 export { default as Score } from "./Score";
 export { default as About } from "./About";
+export { default as Highscore } from "./Highscore";
