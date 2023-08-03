b975c9438cdb82288e588418620930dc6fe40e85
Alexander Ballauf <ballaual@gmail.com>
2023-08-03 19:44:48 +0200

    Feat: implement usage of custom fonts

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import StackNavigator from "./navigation/StackNavigator";
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'BungeeShade': require('./assets/fonts/BungeeShade-Regular.ttf'),
+    'BungeeSpice': require('./assets/fonts/BungeeSpice-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <StackNavigator />
   );
