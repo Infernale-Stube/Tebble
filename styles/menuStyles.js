89c56be9ee3015ec90b273cbb941cd64762b47bb
Alexander Ballauf <ballaual@gmail.com>
2023-08-03 15:44:31 +0200

    Refactor: updated menu styling

--- styles/menuStyles.js
+++ styles/menuStyles.js
@@ -1,40 +1,46 @@
 import { StyleSheet } from 'react-native';
 
 const menuStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-    },
-    logoContainer: {
-      backgroundColor: '#ffbd84',
-      alignItems: 'center',
-    },
-    logo: {
-      alignItems: 'center',
-      height: 200,
-      width: 200,
-      marginTop: 50,
-    },
-    buttonContainer: {
-      flex: 3,
-      backgroundColor: '#ffbd84',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      width: 250,
-      height: 50,
-      backgroundColor: '#000',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginBottom: 15,
-      borderRadius: 10,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  logoTitleContainer: {
+    backgroundColor: '#ffbd84',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  logo: {
+    height: 150,
+    width: 150,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flex: 3,
+    backgroundColor: '#ffbd84',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: 250,
+    height: 60,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 15,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default menuStyles;
