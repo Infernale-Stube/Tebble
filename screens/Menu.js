284db656f628d48f6f44f43980506692ff86d6cb
Alexander Ballauf <ballaual@gmail.com>
2023-08-03 13:07:08 +0200

    Refactor: updating menu screen/styles

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,11 +1,45 @@
+
 import React from 'react';
-import { View, Text } from 'react-native';
-import styles from "../styles/menuStyles"
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { globalStyles, menuStyles } from "../styles"
+import { useNavigation } from '@react-navigation/native';
 
 export default function Menu() {
+  
+  const navigation = useNavigation();
+
+  const navigateToAbout = () => {
+    navigation.navigate('About');
+  };
+
+  const navigateToSettings = () => {
+    navigation.navigate('Settings');
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text>Menu Screen</Text>
+    <View style={globalStyles.container}>
+      <View style={menuStyles.logoContainer}>
+        <Image source={require('../assets/icon.png')} style={menuStyles.logo}/>
+      </View>
+
+      <View style={menuStyles.buttonContainer}>
+        <TouchableOpacity style={menuStyles.button}>
+        
+          <Text style={menuStyles.buttonText}>Campaign Mode</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={menuStyles.button}>
+          <Text style={menuStyles.buttonText}>Random Mode</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={menuStyles.button} onPress={navigateToAbout}>
+          <Text style={menuStyles.buttonText}>About</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={menuStyles.button} onPress={navigateToSettings}>
+          <Text style={menuStyles.buttonText}>Settings</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- styles/menuStyles.js
+++ styles/menuStyles.js
@@ -1,12 +1,40 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
+const menuStyles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
+    },
+    logoContainer: {
+      backgroundColor: '#ffbd84',
+      alignItems: 'center',
+    },
+    logo: {
+      alignItems: 'center',
+      height: 200,
+      width: 200,
+      marginTop: 50,
+    },
+    buttonContainer: {
+      flex: 3,
+      backgroundColor: '#ffbd84',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    button: {
+      width: 250,
+      height: 50,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 15,
+      borderRadius: 10,
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
   });
 
-export default styles;
+export default menuStyles;
