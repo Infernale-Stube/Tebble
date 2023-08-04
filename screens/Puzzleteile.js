4a5563b9157bde11a344bf748ee8baef3aa4a5be
Justdom22 <109627807+Justdom22@users.noreply.github.com>
2023-08-04 15:21:02 +0200

    removed unnecessary code
    removed PuzzlePiece and DraggablePiece as they are no longer needed and therefore reworked Puzzleteile.js to display all pieces without those functions

--- screens/Puzzleteile.js
+++ screens/Puzzleteile.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { DraggablePiece } from "../components/figures";
 import { pieces } from "../components/figures";
 
 export default function Puzzleteile() {
   const pieceKeys = Object.keys(pieces);
+  const piecesInRows = splitIntoRows(pieceKeys, 3);
+
   return (
     <View style={styles.container}>
-      {Array.from({ length: Math.ceil(pieceKeys.length / 3) }, (_, rowIndex) => (
+      {piecesInRows.map((row, rowIndex) => (
         <View key={rowIndex} style={styles.row}>
-          {pieceKeys.slice(rowIndex * 3, rowIndex * 3 + 3).map((key, colIndex) => (
-            <View key={colIndex} style={styles.cell}>
-              <DraggablePiece piece={pieces[key]} />
+          {row.map((key) => (
+            <View key={key} style={styles.piece}>
+              {renderPiece(pieces[key])}
             </View>
           ))}
         </View>
@@ -20,18 +21,43 @@ export default function Puzzleteile() {
   );
 }
 
+const splitIntoRows = (arr, size) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+};
+
+const renderPiece = (currentPiece) => {
+  return currentPiece.map((row, rowIndex) => (
+    <View key={rowIndex} style={{ flexDirection: 'row' }}>
+      {row.map((cell, colIndex) => (
+        <View
+          key={colIndex}
+          style={{
+            width: 30,
+            height: 30,
+            backgroundColor: cell ? 'red' : 'transparent',
+          }}
+        />
+      ))}
+    </View>
+  ));
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     marginBottom: 20,
   },
-  cell: {
-    flex: 1,
-    alignItems: 'center',
+  piece: {
+    marginRight: 10,
   },
 });
