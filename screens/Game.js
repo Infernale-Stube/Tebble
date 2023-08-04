f171ae96ab55e6df63145fc92f3ff2df1f2c0b28
Justdom22 <109627807+Justdom22@users.noreply.github.com>
2023-08-04 14:06:45 +0200

    pieces snap correctly now
    pieces snap into the grid, can't be rotated as of now and just 1 piece spawns in the grid

--- screens/Game.js
+++ screens/Game.js
@@ -1,73 +1,99 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { DraggablePiece } from "../components/figures";
+import React, { useRef, useState } from 'react';
+import { View, PanResponder, StyleSheet, Animated } from 'react-native';
 import { pieces } from "../components/figures";
 
-const gridSize = 15;
-const cellSize = 30;
+export default function Game() {
 
-const Game = () => {
-  const renderGrid = () => {
-    let rows = [];
-    for (let i = 0; i < gridSize; i++) {
-      let cols = [];
-      for (let j = 0; j < gridSize; j++) {
-        cols.push(
-          <View key={j} style={styles.cell} />
-        );
-      }
-      rows.push(
-        <View key={i} style={styles.row}>{cols}</View>
-      );
-    }
-    return rows;
-  };
+  function renderPiece(piece) {
+    return piece.map((row, rowIndex) => (
+      <View key={rowIndex} style={{ flexDirection: 'row' }}>
+        {row.map((cell, colIndex) => (
+          <View
+            key={colIndex}
+            style={{
+              width: 30,
+              height: 30,
+              backgroundColor: cell ? 'blue' : 'transparent',
+            }}
+          />
+        ))}
+      </View>
+    ));
+  }
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      pan.setOffset({ x: pan.x._value, y: pan.y._value });
+      pan.setValue({ x: 0, y: 0 });
+    },
+    onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
+      useNativeDriver: false,
+    }),
+    onPanResponderRelease: () => {
+      pan.flattenOffset();
+      const snapToValue = {
+        x: Math.round(pan.x._value / 30) * 30,
+        y: Math.round(pan.y._value / 30) * 30,
+      };
+
+      Animated.spring(pan, {
+        toValue: snapToValue,
+        useNativeDriver: false,
+      }).start();
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <View style={styles.gridContainer}>
-        {renderGrid()}
+      <View style={styles.grid}>
+        {[...Array(10).keys()].map((_, rowIndex) => (
+          <View key={rowIndex} style={styles.row}>
+            {[...Array(10).keys()].map((_, colIndex) => (
+              <View key={colIndex} style={styles.cell} />
+            ))}
+          </View>
+        ))}
+        <Animated.View
+  {...panResponder.panHandlers}
+  style={[pan.getLayout(), styles.pieceContainer]}
+>
+  {renderPiece(pieces.piece2)}
+</Animated.View>
       </View>
-      {Object.keys(pieces).map((key, index) => (
-        <View key={index} style={styles.pieceCell}>
-          <DraggablePiece piece={pieces[key]} initialX={(index % 3) * cellSize} initialY={Math.floor(index / 3) * cellSize} />
-        </View>
-      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  pieceContainer: {
+    position: 'absolute',
+  },
   container: {
     flex: 1,
-    flexDirection: 'column',
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  gridContainer: {
-    flex: 3,
-    padding: 10,
-  },
-  piecesContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+  grid: {
+    width: 300,
+    height: 300,
   },
   row: {
     flexDirection: 'row',
   },
   cell: {
-    width: cellSize,
-    height: cellSize,
-    borderWidth: 0.5,
+    width: 30,
+    height: 30,
     borderColor: 'black',
-  },
-  pieceCell: {
-    width: cellSize,
-    height: cellSize,
     borderWidth: 0.5,
-    borderColor: 'blue', //change to transparent again after debugging
-    margin: 10,
+  },
+  square: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'red',
+    position: 'absolute',
   },
 });
-
-export default Game;
