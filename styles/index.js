9347a1ba3ca6a1e7b62f02e25193c44a4e48dca8
Alexander Ballauf <ballaual@gmail.com>
2023-08-05 19:12:05 +0200

    Refactor: typo hishscore -> highscore

--- styles/index.js
+++ styles/index.js
@@ -2,4 +2,4 @@ export { default as globalStyles } from "./globalStyles";
 export { default as menuStyles } from "./menuStyles";
 export { default as settingsStyles } from "./settingsStyles";
 export { default as aboutStyles } from "./aboutStyles";
-export { default as hishscoreStyles } from "./hishscoreStyles";
+export { default as highscoreStyles } from "./highscoreStyles";
