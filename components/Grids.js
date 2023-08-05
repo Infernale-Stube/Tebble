645584c53541d3ae0cdd5c59d3b937015298ee4d
Alexander Ballauf <ballaual@gmail.com>
2023-08-05 19:13:08 +0200

    Refactor: remove unused imports

--- components/Grids.js
+++ components/Grids.js
@@ -1,9 +1,3 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View, StyleSheet, PanResponder, Animated, TouchableOpacity, Dimensions } from 'react-native';
-import { globalStyles, aboutStyles } from '../styles';
-
-const cellSize = 30;
-
 export const gridData = {
   grid1: [
     [1, 1, 1, 1, 1, 1, 1, 1],
