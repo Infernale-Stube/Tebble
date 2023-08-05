59f2f68349f815a481650448523d0d168007f30f
Alexander Ballauf <ballaual@gmail.com>
2023-08-05 19:35:37 +0200

    Refactor: removed unused stylesheet import

--- screens/Overview.js
+++ screens/Overview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { globalStyles, overviewStyles } from "../styles"
 import { Pieces } from "../components/Pieces";
 
