aa0110702568a1a6de86385a16cf8aae447168c8
Alexander Ballauf <ballaual@gmail.com>
2023-08-05 19:35:04 +0200

    Refactor: created separate overview stylesheet

--- styles/index.js
+++ styles/index.js
@@ -3,3 +3,4 @@ export { default as menuStyles } from "./menuStyles";
 export { default as settingsStyles } from "./settingsStyles";
 export { default as aboutStyles } from "./aboutStyles";
 export { default as highscoreStyles } from "./highscoreStyles";
+export { default as overviewStyles } from "./overviewStyles";
