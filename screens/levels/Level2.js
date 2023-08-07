5b4d62a8095c8801f1387bdd035be0a6f715dfd0
Alexander Ballauf <ballaual@gmail.com>
2023-08-07 14:25:56 +0200

    Feat: implementing level screens

---
+++ screens/levels/Level2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Level2() {
+  return (
+    <View>
+      <Text>Level2 Screen</Text>
+    </View>
+  );
+}
