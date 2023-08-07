3369274b64ce3f78a3651441ae469da301491f77
Alexander Ballauf <ballaual@gmail.com>
2023-08-07 14:26:36 +0200

    Refactor: update navigation file to work with nested navigation

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,22 +1,37 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Menu, Settings, About, Overview, Highscore, Game } from "../screens"
+import { Menu, Settings, About, Overview, Highscore, Game, Campaign } from "../screens";
 import { StatusBar } from 'expo-status-bar';
 
+const levelScreens = require('../screens/levels');
 const Stack = createNativeStackNavigator();
+
+function LevelNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {Object.keys(levelScreens).map(levelName => (
+        <Stack.Screen key={levelName} name={levelName} component={levelScreens[levelName]} />
+      ))}
+    </Stack.Navigator>
+  );
+}
 
 function StackNavigator() {
   return (
     <NavigationContainer>
       <StatusBar hidden={true}/>
       
-      <Stack.Navigator>
-        <Stack.Screen name="Menu" options={{ headerShown: false }} component={Menu} />
-        <Stack.Screen name="Settings" options={{ headerShown: false }} component={Settings} />
-        <Stack.Screen name="Game" options={{ headerShown: false }} component={Game} />
-        <Stack.Screen name="About" options={{ headerShown: false }} component={About} />
-        <Stack.Screen name="Highscore" options={{ headerShown: false }} component={Highscore} />
-        <Stack.Screen name="Overview" options={{ headerShown: false }} component={Overview} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Menu" component={Menu} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="Game" component={Game} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Highscore" component={Highscore} />
+        <Stack.Screen name="Overview" component={Overview} />
+        <Stack.Screen name="Campaign" component={Campaign} />
+        
+        <Stack.Screen name="LevelNavigator" component={LevelNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
