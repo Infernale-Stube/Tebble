d412ce13d6c02b41f48cebd7d0346cceefeb122d
Alexander Ballauf <ballaual@gmail.com>
2023-08-10 15:30:35 +0200

    Refactor: updating level1 screen to current Game.js

--- screens/levels/Level1.js
+++ screens/levels/Level1.js
@@ -1,26 +1,27 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { View, PanResponder, StyleSheet, Animated } from 'react-native';
-import { Pieces } from "../../components/Pieces";
-import { Grids } from "../../components/Grids";
+import { View, PanResponder, Animated } from 'react-native';
+import { Grids } from '../../components/Grids';
+import { globalStyles, gameStyles } from '../../styles';
 
 export default function Level1() {
-  const pieceNames = ['piece1', 'piece2', 'piece4']; // define the pieces to be spawned
-  const [currentGrid, setCurrentGrid] = useState('grid2'); // define the grid to be displayed
+  const [currentGrid, setCurrentGrid] = useState('grid2');
 
-  const [isFilled, setIsFilled] = useState(false);
-
-  const [activePieces, setActivePieces] = useState(pieceNames.map(name => ({
-    piece: Pieces[name],
-    pan: useRef(new Animated.ValueXY()).current,
-    lastTap: null,
-  })));
+  const [activePieces, setActivePieces] = useState(
+    Grids[currentGrid].pieces.map(piece => ({
+      piece,
+      pan: useRef(new Animated.ValueXY()).current,
+      lastTap: null,
+    }))
+  );
 
   const rotatePiece = (index) => {
-    setActivePieces(Pieces => Pieces.map((p, i) => {
+    setActivePieces(activePieces.map((p, i) => {
       if (i !== index) return p;
       return {
         ...p,
-        piece: p.piece.map((row, rowIndex) => p.piece.map((col) => col[rowIndex])).reverse()
+        piece: p.piece.map((row, rowIndex) =>
+          p.piece.map((col) => col[rowIndex])
+        ).reverse()
       };
     }));
   };
@@ -34,7 +35,7 @@ export default function Level1() {
             style={{
               width: 30,
               height: 30,
-              backgroundColor: cell ? 'red' : 'transparent', // define color of pieces
+              backgroundColor: cell ? '#FF5733' : 'transparent',
             }}
           />
         ))}
@@ -43,13 +44,13 @@ export default function Level1() {
   );
 
   const renderGrid = () => (
-    Grids[currentGrid].map((row, rowIndex) => (
-      <View key={rowIndex} style={styles.row}>
+    Grids[currentGrid].grid.map((row, rowIndex) => (
+      <View key={rowIndex} style={gameStyles.row}>
         {row.map((cell, colIndex) => (
           <View
             key={colIndex}
             style={[
-              styles.cell,
+              gameStyles.cell,
               {
                 backgroundColor: cell ? 'transparent' : 'transparent',
                 opacity: cell ? 1 : 0,
@@ -62,39 +63,38 @@ export default function Level1() {
   );
 
   const checkIfGridIsFilled = () => {
-    const activePiecesGridCells = activePieces.flatMap(p => (
-      p.piece.flatMap((row, rowIndex) => (
+    const activePiecesGridCells = activePieces.flatMap(p =>
+      p.piece.flatMap((row, rowIndex) =>
         row.map((cell, cellIndex) => cell && ({
           x: Math.round(p.pan.x._value / 30) + cellIndex,
           y: Math.round(p.pan.y._value / 30) + rowIndex,
         }))
-      )).filter(Boolean)
-    ));
+      ).filter(Boolean)
+    );
 
-    const isGridFilled = Grids[currentGrid].every((row, rowIndex) =>
+    const isGridFilled = Grids[currentGrid].grid.every((row, rowIndex) =>
       row.every((cell, cellIndex) =>
         !cell || activePiecesGridCells.some(p => p.x === cellIndex && p.y === rowIndex)
       )
     );
 
-    setIsFilled(isGridFilled);
+    return isGridFilled;
+  };
 
-    if (isGridFilled) {
+  useEffect(() => {
+    if (checkIfGridIsFilled()) {
       console.log('Grid is filled');
     } else {
       console.log('Grid is not filled');
     }
-  };
+  }, [activePieces]);
 
-  useEffect(checkIfGridIsFilled, [activePieces]);
-
-  // dynamic calculation of gridsize
-  const gridWidth = 30 * Grids[currentGrid][0].length;
-  const gridHeight = 30 * Grids[currentGrid].length;
+  const gridWidth = 30 * Grids[currentGrid].grid[0].length;
+  const gridHeight = 30 * Grids[currentGrid].grid.length;
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.grid, { width: gridWidth, height: gridHeight }]}>
+    <View style={[globalStyles.container, gameStyles.container]}>
+      <View style={[gameStyles.grid, { width: gridWidth, height: gridHeight }]}>
         {renderGrid()}
         {activePieces.map((p, index) => {
           const panResponder = PanResponder.create({
@@ -121,14 +121,18 @@ export default function Level1() {
                 toValue: snapToValue,
                 useNativeDriver: false,
               }).start();
-              checkIfGridIsFilled();
+              if (checkIfGridIsFilled()) {
+                console.log('Grid is filled');
+              } else {
+                console.log('Grid is not filled');
+              }
             },
           });
           return (
             <Animated.View
               key={index}
               {...panResponder.panHandlers}
-              style={[p.pan.getLayout(), styles.pieceContainer]}
+              style={[p.pan.getLayout(), gameStyles.pieceContainer]}
             >
               {renderPiece(p.piece)}
             </Animated.View>
@@ -138,28 +142,3 @@ export default function Level1() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  pieceContainer: {
-    position: 'absolute',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f0f0',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  grid: {
-    width: 300,
-    height: 300,
-  },
-  row: {
-    flexDirection: 'row',
-  },
-  cell: {
-    width: 30,
-    height: 30,
-    borderColor: 'black',
-    borderWidth: 0.5,
-  },
-});
