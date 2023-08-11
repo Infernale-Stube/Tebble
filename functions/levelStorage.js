bcac17400617fdce09a53652b9b8ec205ab59d59
Alexander Ballauf <ballaual@gmail.com>
2023-08-11 11:19:37 +0200

    Refactor: updated levelStorage

--- functions/levelStorage.js
+++ functions/levelStorage.js
@@ -1,12 +1,36 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const CAMPAIGN_LEVEL = '@campaignLevel';
+const CURRENT_LEVEL_KEY = 'currentLevel';
 
-export const saveCampaignLevel = async (level) => {
-  await AsyncStorage.setItem(CAMPAIGN_LEVEL, level.toString());
+const isInitialLevelSet = async () => {
+  try {
+    const value = await AsyncStorage.getItem(CURRENT_LEVEL_KEY);
+    return value !== null;
+  } catch (error) {
+    console.error('Error checking initial value:', error);
+    return false;
+  }
 };
 
-export const loadCampaignLevel = async () => {
-  const level = await AsyncStorage.getItem(CAMPAIGN_LEVEL);
-  return level ? parseInt(level) : 1;
+export const setInitialLevel = async () => {
+  const initialValueSet = await isInitialLevelSet();
+
+  if (!initialValueSet) {
+    try {
+      await AsyncStorage.setItem(CURRENT_LEVEL_KEY, '1');
+    } catch (error) {
+      console.error('Error setting initial value:', error);
+    }
+  }
+};
+
+export const unlockNextLevel = async () => {
+  try {
+    const currentLevel = await AsyncStorage.getItem(CURRENT_LEVEL_KEY);
+    const newLevel = parseInt(currentLevel, 10) + 1;
+    await AsyncStorage.setItem(CURRENT_LEVEL_KEY, newLevel.toString());
+    return newLevel;
+  } catch (error) {
+    console.error('Error unlocking next level:', error);
+  }
 };
