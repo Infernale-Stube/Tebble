27dc82b856facc4e0c5c77db8bc6818a0e4d6041
Alexander Ballauf <ballaual@gmail.com>
2023-08-11 11:20:15 +0200

    Refactor: updated global background color

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D2D2CF',
+    backgroundColor: '#FFFFF',
   },
 });
 
